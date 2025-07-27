88cfd1291d7f0a12050ae177e79c930a5489ccb1
yamaniii <elyamanyaa51@gmail.com>
2025-07-27 21:16:28 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main () {
+  runApp( NotesApp(), );
+}
+class NotesApp extends StatelessWidget {
+  const NotesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
