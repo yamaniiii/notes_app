4d5359a5ee9ca070a21e78a360f35f4c8b33552d
yamaniii <elyamanyaa51@gmail.com>
2025-07-29 20:34:30 +0300

    add floating action button

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:notesapp/views/widgets/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){} ,
+        child: const Icon(Icons.add),
+          backgroundColor: Colors.deepPurpleAccent,
+        shape: const CircleBorder(),
+      ),
+      body: const NotesViewBody(),
+    );
   }
 }
