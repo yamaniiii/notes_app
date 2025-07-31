c684778a83d98c407a8286ac17c46fafe54fecef
yamaniii <elyamanyaa51@gmail.com>
2025-07-31 20:29:52 +0300

    create edit note view

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:notesapp/views/widgets/custom_text_field.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        children: const [
+          SizedBox(height: 32,),
+          CustomTextField( hint: "Title" ),
+          SizedBox(height: 32,),
+          CustomTextField( hint: "Content" , maxLines: 5, ),
+        ],
+      ),
+    );
   }
 }
